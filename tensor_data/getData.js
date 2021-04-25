d70117712d7c59f0bd99a6e0e33c8d089ce1711b
Tomas Hartman <tomash@xcroco.com>
2021-04-26 01:06:17 +0200

    dev (tensorflow): prepare data for categorization

---
+++ tensor_data/getData.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const fs = require('fs');
+
+const {db} = require('../scripts/dbconnect/index');
+const { normalizeAr } = require('../scripts/normalize');
+
+const outputFileName = path.resolve(__dirname, './categories_input_data.json');
+const charsTableFile = path.resolve(__dirname, './chars_convert_table.json');
+const writeStream2 = (filename) => fs.createWriteStream(filename);
+const sql = 'SELECT ar, cat_ids FROM vocabulary WHERE cat_ids NOT null';
+
+db.all(sql, (err, rows) => {    
+    if(err) throw new Error(err);
+
+    // const output = getData(rows);
+    const output = processChars(rows);
+
+    writeStream2(charsTableFile).write(JSON.stringify(Array.from(output)), (err) => {
+        if(err) throw new Error(err);
+    }
+    );
+
+    // writeStream2(outputFileName).write(JSON.stringify(output), (err) => {
+    //     if(err) throw new Error(err);
+    // });
+});
+
+const processChars = (rows) => {
+    const letters = new Set();
+
+    for(let j in rows) {
+        const row = rows[j];
+
+        const {ar} = row;
+
+        const letterMap = [...normalizeAr(ar)];
+
+        letterMap.forEach((char) => {
+            letters.add(char.normalize('NFKD'));
+        });
+    }
+    console.log(letters);
+
+    return letters;
+};
+
+const getData = (rows) => {
+    let outputData = [];
+
+    for(let j in rows) {
+        const row = rows[j];
+
+        const {ar, cat_ids} = row;
+        const cats = JSON.parse(cat_ids);
+    
+        // rule out phrases
+        if(ar.split(' ').length > 1) continue;
+    
+        // each category has its own entry
+        for(let i in cats) {
+            const cleanAr = normalizeAr(ar);
+    
+            outputData.push([cleanAr, cats[i]]);
+        }
+    }
+
+    return outputData;
+};
+
+// (async () => {
+//     await promise;
+    
+//     
+// })();
+
